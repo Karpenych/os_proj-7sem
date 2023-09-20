e8d60f56ebaadef12bb0c891af9b130f136eecde
karpenych <dmitriy52444@gmail.com>
2023-09-20 09:12:33 +0300

    finish lab1_2

--- lr1_1/Form1.Designer.cs
+++ lr1_1/Form1.Designer.cs
@@ -34,7 +34,15 @@
             textBox2 = new TextBox();
             textBox1 = new TextBox();
             button1 = new Button();
+            groupBox2 = new GroupBox();
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
@@ -48,7 +56,7 @@
             groupBox1.Controls.Add(button1);
             groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(484, 313);
+            groupBox1.Size = new Size(592, 308);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Информация о структуре реестра";
@@ -98,16 +106,91 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // groupBox2
+            // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox2.Controls.Add(listBox1);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Location = new Point(12, 350);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(592, 452);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Вывод названий и значений ветки реестра";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(23, 135);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(534, 229);
+            listBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(22, 94);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(176, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Выбор параметра";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(384, 32);
+            button4.Name = "button4";
+            button4.Size = new Size(173, 23);
+            button4.TabIndex = 2;
+            button4.Text = "С выбором параметра";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(204, 32);
+            button3.Name = "button3";
+            button3.Size = new Size(174, 23);
+            button3.TabIndex = 1;
+            button3.Text = "HKEY_CURRENT_USER";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(17, 32);
+            button2.Name = "button2";
+            button2.Size = new Size(181, 23);
+            button2.TabIndex = 0;
+            button2.Text = "HKEY_LOCAL_MACHINE";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(508, 337);
+            ClientSize = new Size(616, 814);
+            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Карпенко ЛР1";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -120,5 +203,12 @@
         private Label label1;
         private TextBox textBox2;
         private TextBox textBox1;
+        private GroupBox groupBox2;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Button button4;
+        private Button button3;
+        private Button button2;
     }
 }
--- lr1_1/Form1.cs
+++ lr1_1/Form1.cs
@@ -1,4 +1,5 @@
 using Microsoft.Win32;
+using System.Windows.Forms;
 
 namespace os_lr_1
 {
@@ -28,6 +29,54 @@ namespace os_lr_1
                 textBox1.Text += regKey.Name + "\r\n";
                 textBox2.Text += regKey.SubKeyCount + "\r\n";
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            RegistryKey rKey1 = Registry.LocalMachine;
+            string key2 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\\Folder\\";
+            RegistryKey rKey2 = rKey1.OpenSubKey(key2);
+            string[] vNames = rKey2.GetValueNames();
+
+            foreach (var name in vNames)
+                listBox1.Items.Add(name + "\t\t" + rKey2.GetValue(name).ToString());
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            RegistryKey rKey1 = Registry.CurrentUser;
+            string key2 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\\";
+            RegistryKey rKey2 = rKey1.OpenSubKey(key2);
+            string[] vNames = rKey2.GetValueNames();
+
+            foreach (var name in vNames)
+                listBox1.Items.Add(name + "\t\t\t" + rKey2.GetValue(name).ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            RegistryKey rKey1 = Registry.CurrentUser;
+            string key2 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\";
+            RegistryKey rKey2 = rKey1.OpenSubKey(key2);
+            string key3 = comboBox1.Text;
+            try
+            {
+                RegistryKey rKey3 = rKey2.OpenSubKey(key3);
+                string[] vNames = rKey3.GetValueNames();
+
+                foreach (var name in vNames)
+                    listBox1.Items.Add(name + "\t\t\t" + rKey2.GetValue(name).ToString());
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add(ex.Message);
+            }
+            
+
         }
     }
 }
